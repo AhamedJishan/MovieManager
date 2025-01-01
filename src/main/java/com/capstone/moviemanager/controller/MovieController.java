1da2e88f152722d4da3e337260cd2b91f946f197
Jishan Ahamed <jjahamed33@gmail.com>
2025-01-01 21:05:05 +0530

    MovieController modified: added rest of the crud functionalities

--- src/main/java/com/capstone/moviemanager/controller/MovieController.java
+++ src/main/java/com/capstone/moviemanager/controller/MovieController.java
@@ -3,8 +3,7 @@ package com.capstone.moviemanager.controller;
 import com.capstone.moviemanager.model.Movie;
 import com.capstone.moviemanager.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,20 +13,42 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    // HOME
     @GetMapping(value = {"/home", "/"})
     public String Home()
     {
         return "Welcome to Jishan's Movie Management system!";
     }
 
+    // GET ALL
     @GetMapping("/movies")
     public List<Movie> getAllMovies()
     {
         return movieService.getAllMovies();
     }
 
+    // GET ONE BY ID
+    @GetMapping("/movie/{id}")
+    public Movie getMovieById(@PathVariable int id) {
+        return movieService.getMovieById(id);
+    }
 
-    // TODO: CREATE
-    // TODO: UPDATE
-    // TODO: DELETE
+    // CREATE
+    @PostMapping("/movie")
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    // UPDATE
+    @PutMapping("/movie")
+    public Movie updateMovie(@RequestBody Movie movie) {
+        return movieService.updateMovie(movie);
+    }
+
+    // DELETE
+    @DeleteMapping("/movie/{id}")
+    public String deleteMovie(@PathVariable int id) {
+        movieService.deleteMovie(id);
+        return "Deleted movie with id: " + id;
+    }
 }
