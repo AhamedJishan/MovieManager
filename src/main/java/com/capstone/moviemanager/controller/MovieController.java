6fcf5568a2ac778027363829fa6eb818c121330c
Jishan Ahamed <jjahamed33@gmail.com>
2025-01-01 18:20:11 +0530

    MovieController modified: getAllMovies() added

--- src/main/java/com/capstone/moviemanager/controller/MovieController.java
+++ src/main/java/com/capstone/moviemanager/controller/MovieController.java
@@ -1,14 +1,33 @@
 package com.capstone.moviemanager.controller;
 
+import com.capstone.moviemanager.model.Movie;
+import com.capstone.moviemanager.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
 
     @GetMapping(value = {"/home", "/"})
     public String Home()
     {
         return "Welcome to Jishan's Movie Management system!";
     }
+
+    @GetMapping("/movies")
+    public List<Movie> getAllMovies()
+    {
+        return movieService.getAllMovies();
+    }
+
+
+    // TODO: CREATE
+    // TODO: UPDATE
+    // TODO: DELETE
 }
