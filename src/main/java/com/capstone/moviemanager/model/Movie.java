de08d3bd2990e28b2c07bed5c3a34e8846d2871f
Jishan Ahamed <jjahamed33@gmail.com>
2025-01-01 17:50:17 +0530

    Movie class entity added

---
+++ src/main/java/com/capstone/moviemanager/model/Movie.java
@@ -0,0 +1,28 @@
+package com.capstone.moviemanager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    private String tagline;
+
+    private String overview;
+
+    private double revenue;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", length = 20)
+    private MovieStatus status;
+}
