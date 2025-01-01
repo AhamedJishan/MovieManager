417296a6a8f9b0f18d95158952df6ef9b62faee9
Jishan Ahamed <jjahamed33@gmail.com>
2025-01-01 21:04:16 +0530

    MovieService modified: Added rest of the crud functions

--- src/main/java/com/capstone/moviemanager/service/MovieService.java
+++ src/main/java/com/capstone/moviemanager/service/MovieService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +16,22 @@ public class MovieService {
 
     public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Movie getMovieById(int id) {
+        Optional<Movie> movie = movieRepository.findById(id);
+        return movie.orElseThrow(() -> new RuntimeException("No movie found with id: " + id));
+    }
+
+    public Movie createMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public Movie updateMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public void deleteMovie(int id) {
+        movieRepository.deleteById(id);
     }
 }
