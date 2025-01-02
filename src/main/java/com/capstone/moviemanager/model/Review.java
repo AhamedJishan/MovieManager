8e1cf06c552211aa84f27e911c4032d545279850
Jishan Ahamed <jjahamed33@gmail.com>
2025-01-02 10:46:25 +0530

    Review Entity created

---
+++ src/main/java/com/capstone/moviemanager/model/Review.java
@@ -0,0 +1,29 @@
+package com.capstone.moviemanager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String author;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Lob
+    private String content;
+}
